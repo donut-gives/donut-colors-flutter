91523f4fda28ea0904c327f5eedcce8e55f3c562
Ayush Suman <ayush231suman@gmail.com>
2022-03-28 16:54:55 +0530

    Donut Colors

---
+++ lib/donutcolors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+extension DonutColors on Colors {
+  static const Color blue = Color(0xff1E7BEE);
+  static const Color black = Color(0xff020913);
+  static const Color display = Color(0xff28282A);
+  static const Color pink = Color(0xffEF5DA8);
+  static const Color lightBlue = Color(0xff1D9BF0);
+  static const Color lightBlue2 = Color(0xffB3D3F9);
+  static const Color lightBlue1 = Color(0xffD8E9FD);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color offWhite = Color(0xffFAFAFA);
+  static const Color grey = Color(0x4cD4D4D4);
+  static const Color grey1 = Color(0xff020913);
+  static const Color grey2 = Color(0x99020913);
+  static const Color shadow = Color(0xff28282A);
+}
+
