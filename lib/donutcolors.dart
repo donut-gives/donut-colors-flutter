27675cd017fb5dd088bb4b419d4a539b5af03ee2
Ayush Suman <ayush231suman@gmail.com>
2022-04-25 14:39:05 +0530

    Documentation

--- lib/donutcolors.dart
+++ lib/donutcolors.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
 
-extension DonutColors on Colors {
-  static const Color blue = Color(0xff1E7BEE);
-  static const Color black = Color(0xff020913);
-  static const Color display = Color(0xff28282A);
-  static const Color pink = Color(0xffEF5DA8);
-  static const Color lightBlue = Color(0xff1D9BF0);
-  static const Color lightBlue2 = Color(0xffB3D3F9);
-  static const Color lightBlue1 = Color(0xffD8E9FD);
-  static const Color white = Color(0xffFFFFFF);
-  static const Color offWhite = Color(0xffFAFCFF);
-  static const Color grey = Color(0xFFD4D4D4);
-  static const Color grey1 = Color(0xFF676B71);
-  static const Color grey2 = Color(0xFF9A9DA1);
-  static const Color shadow1 = Color(0x3328282A);
-  static const Color shadow2 = Color(0x26000000);
+class DonutColor extends Color {
+  const DonutColor(int value) : super(value);
+
+  operator [](double opacity) => withOpacity(opacity);
 }
 
+
+
+/// Color library of Donut
+/// Use this library of colors to build your Flutter applications.
+/// Example - To use color [blue] with 30% opacity
+/// DonutColors.blue[0.3]
+class DonutColors {
+  DonutColors._();
+  
+  static const DonutColor blue = DonutColor(0xFF1E7BEE);
+  static const DonutColor black = DonutColor(0xFF020913);
+  static const DonutColor display = DonutColor(0xFF28282A);
+  static const DonutColor pink = DonutColor(0xFFEF5DA8);
+  static const DonutColor lightBlue = DonutColor(0xFF1D9BF0);
+  static const DonutColor lightBlue2 = DonutColor(0xFFB3D3F9);
+  static const DonutColor lightBlue1 = DonutColor(0xFFD8E9FD);
+  static const DonutColor white = DonutColor(0xFFFFFFFF);
+  static const DonutColor offWhite = DonutColor(0xFFFAFCFF);
+  static const DonutColor grey = DonutColor(0xFFD4D4D4);
+  static const DonutColor grey1 = DonutColor(0xFF676B71);
+  static const DonutColor grey2 = DonutColor(0xFF9A9DA1);
+  static const DonutColor shadow1 = DonutColor(0x3328282A);
+  static const DonutColor shadow2 = DonutColor(0x26000000);
+}
