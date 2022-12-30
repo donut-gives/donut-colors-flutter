c40f25fa86ce2fa0866f9ca8821df1386c160e1a
Ayush Suman <ayush231suman@gmail.com>
2022-12-30 15:09:32 +0530

    Remove Implemented Color from Lib

--- lib/donutcolors.dart
+++ lib/donutcolors.dart
@@ -90,28 +90,3 @@ class DonutColors {
   final DonutColorSwatch<Shadows> shadow;
   final DonutColor error;
 }
-
-final DonutDyes = DonutColors(
-    background: const DonutColor(0xFFFAFAFA),
-    surface: const DonutColor(0xFFFFFFFF),
-    primary: const DonutColorSwatch.primary(main: 0xFF1E7BEE,
-      light1: const DonutColor(0xFF549BF2),
-      light2: const DonutColor(0xFF7AB1F5),
-      ultraLight: const DonutColor(0xFFA0C8F8)
-    ),
-    secondary: const DonutColorSwatch.secondary(main: 0xFFEF5DA8,
-      variant: const DonutColor(0xFFF6A2CD)
-    ),
-    neutral: const DonutColorSwatch.neutral(main: 0xFFE0E0E0,
-      variant: const DonutColor(0xFFD0D0D0)
-    ),
-    text: const DonutColorSwatch.Text(main: 0xFF303030,
-      inactive: const DonutColor(0xFF7A7A7A),
-      inverted: const DonutColor(0xFFFFFFFF),
-    ),
-    shadow: const DonutColorSwatch.shadows(light: 0x15000000,
-        medium: const DonutColor(0x1F000000),
-        dark: const DonutColor(0x28000000)
-    ),
-  error: const DonutColor(0xFFEA4335)
-);
